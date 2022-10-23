fac095647eb99b030be583abab0ba28e321981ac
joelcasti <joelwilliamcastillo@gmail.com>
2022-10-23 15:44:21 -0300

    Instalacion y configuracion de Sequelize, Instalacion de EJS

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
-const path = require('path');
+const app = express();
+
+app.use(express.urlencoded( {extended : false} ));
+app.use(express.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 const indexRouter = require('./src/routes/indexRouter');
 const userRouter = require('./src/routes/userRouter');
-
-const app = express();
-
-const publicPath = path.resolve(__dirname, './public');
-app.use( express.static(publicPath) );
-
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
-let puerto = process.env.PORT || 3000;
-app.listen(puerto, () => {
-    console.log("Servidor corriendo en el puerto " + puerto);
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Servidor corriendo en marcha");
 });
